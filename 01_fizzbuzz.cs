1f5b2b159a5893c405cf3be6e5f1361d1b115385
kiirdes <caquilalacedric@gmail.com>
2024-03-18 23:26:40 +0800

    dynamic fizz buzz, up to 10 divisors. also accepts CLI args.

--- 01_fizzbuzz.cs
+++ 01_fizzbuzz.cs
@@ -1,36 +1,70 @@
-﻿int? ParseUserInput(String userInput)
+﻿using System.Collections;
+
+class ExerciseClass
 {
-    int userMax = -1;
-
-    userInput = userInput.Trim();
-    if (!int.TryParse(userInput, out userMax) || userMax < 1)
-        return null;
-
-    return userMax;
-}
-
-int main()
-{
-    String userInput = "";
-    int? userMax;
-    int fizz = 2;
-    int buzz = 3;
-
-    Console.WriteLine("Give me a number and I'll fizz buzz for {0} and {1}.", fizz, buzz);
-
-    userInput = Console.ReadLine() ?? "0";
-    if ((userMax = ParseUserInput(userInput)) == null) return -1;
-
-    Console.WriteLine();
-    for (int i = 1; i < userMax; i++)
+    static int? ParseUserInput(String userInput)
     {
-        if (0 == i % fizz) Console.Write("fizz");
-        if (0 == i % buzz) Console.Write("buzz");
-        if (0 != i % fizz && 0 != i % buzz) Console.Write(i);
-        Console.WriteLine();
+        int userMax = -1;
+
+        userInput = userInput.Trim();
+        if (!int.TryParse(userInput, out userMax) || userMax < 1)
+            return null;
+
+        return userMax;
     }
-    
-    return 0;
+
+    static String[] keywords = ["fizz", "buzz", "doom", "jong", "spow", "reng", "gaow", "pirt", "klek", "zdyn"];
+
+    static void Main(String[] args)
+    {
+        String userInput = "";
+        int userMax = 1;
+        List<int?> divisors = [];
+
+        if(args.Length == 0) {
+            Console.WriteLine("Input max number.");
+            userInput = Console.ReadLine() ?? "1";
+            userMax = ParseUserInput(userInput) ?? 2;
+        } else
+        {
+            int.TryParse(args[0], out userMax);
+        }
+
+        if (args.Length <= 1) {
+            Console.WriteLine("Input a divisor.");
+            userInput = Console.ReadLine() ?? "1";
+            divisors.Add(ParseUserInput(userInput) ?? 2);
+        }
+
+        if(args.Length > 1) {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i == 0) continue;
+                divisors.Add(ParseUserInput(args[i]));
+            }
+        }
+
+        Console.WriteLine("Fizz buzzing for {0}", userMax);
+        Console.Write("Divisors are: ");
+        foreach (int divisor in divisors) Console.Write("{0} ", divisor);
+        Console.WriteLine();
+        for (int i = 1; i < userMax + 1; i++)
+        {
+            bool hasMatched = false;
+            for(int j = 0; j < divisors.Count; j++)
+            {
+                if (0 == i % divisors[j])
+                {
+                    Console.Write(keywords[j]);
+                    hasMatched = true;
+                }
+            }
+            if (!hasMatched) {
+                Console.Write(i);
+            };
+            Console.WriteLine();
+        }
+        
+    }
 }
 
-main();
